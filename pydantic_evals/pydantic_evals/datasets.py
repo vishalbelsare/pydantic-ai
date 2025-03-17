3970a8ced55f967307ebd46b75837eae6a80feef
David Montague <35119617+dmontagu@users.noreply.github.com>
2025-03-17 08:02:03 -0600

    WIP

--- pydantic_evals/pydantic_evals/datasets.py
+++ pydantic_evals/pydantic_evals/datasets.py
@@ -2,9 +2,12 @@
 from __future__ import annotations as _annotations
 
 import functools
+import inspect
 import sys
 from pathlib import Path
-from typing import Any, ClassVar, Generic, Self
+from typing import Any, ClassVar, Generic, Literal, NotRequired, Self, Union
+
+from pydantic._internal import _typing_extra
 
 if sys.version_info < (3, 11):
     from exceptiongroup import ExceptionGroup
@@ -12,11 +15,11 @@ else:
     ExceptionGroup = ExceptionGroup
 
 import yaml
-from pydantic import BaseModel, Field, TypeAdapter, ValidationError
+from pydantic import BaseModel, Field, ValidationError
 from pydantic_core import to_json, to_jsonable_python
-from typing_extensions import TypeVar
+from typing_extensions import TypedDict, TypeVar
 
-from .assessments.spec import Assessment, AssessmentFunction, AssessmentRegistry, AssessmentSpec, get_default_registry
+from .assessments.spec import Assessment, AssessmentFunction, AssessmentSpec, get_default_registry
 
 InputsT = TypeVar('InputsT', default=dict[str, Any])
 OutputT = TypeVar('OutputT', default=dict[str, Any])
@@ -71,19 +74,19 @@ class Dataset(BaseModel, Generic[InputsT, OutputT, MetadataT], extra='forbid'):
 
     @classmethod
     @functools.cache
-    def type_adapter(cls) -> TypeAdapter[Self]:
-        return TypeAdapter(cls)
+    def _row_type(cls) -> type[EvaluationRow[InputsT, OutputT, MetadataT]]:
+        return EvaluationRow[cls._params]  # type: ignore
 
     @classmethod
     @functools.cache
-    def row_type(cls) -> type[EvaluationRow[InputsT, OutputT, MetadataT]]:
+    def _params(cls) -> tuple[type[InputsT], type[OutputT], type[MetadataT]]:
         for c in cls.__mro__:
             metadata = getattr(c, '__pydantic_generic_metadata__')
-            if len(args := metadata.get('args')) == 3:
-                return EvaluationRow[args]  # type: ignore
+            if len(args := metadata.get('args', ())) == 3:
+                return args
             elif len(args := getattr(c, '__args__', ())) == 3:
-                return EvaluationRow[args]  # type: ignore
-        raise ValueError(f'Could not find the DatasetRow type for {cls}')
+                return args
+        raise ValueError(f'Could not determine the generic parameters for {cls}')
 
     @classmethod
     def assessment(
@@ -96,29 +99,12 @@ class Dataset(BaseModel, Generic[InputsT, OutputT, MetadataT], extra='forbid'):
         cls._assessment_registry[f.__name__] = f
         return f
 
-    @classmethod
-    def from_yaml(cls, path: Path | str = DEFAULT_DATASET_PATH) -> Self:
-        path = cls._get_relative_path(path)
-        if not path.exists():
-            raise FileNotFoundError(f'{cls.__name__} dataset file {path} does not exist')
-
-        raw = path.read_text()
-        loaded = yaml.safe_load(raw)
-        try:
-            result = cls.model_validate(loaded)
-            # result.rows = [cls.serialized_row_type().model_validate(row.model_dump()) for row in result.rows]
-        except ValidationError as e:
-            raise ValueError(
-                f'{cls.__name__} dataset file {path} contains data that does not match the schema:\n{e}.'
-            ) from e
-        return result
-
     def eval_cases(self) -> list[EvaluationRow[InputsT, OutputT, MetadataT]]:
         registry = self.assessment_registry()
 
         evaluation_rows: list[EvaluationRow[InputsT, OutputT, MetadataT]] = []
         errors: list[ValueError] = []
-        row_type = self.row_type()
+        row_type = self._row_type()
         for row in self.rows:
             assessments: list[Assessment[Any, Any, Any]] = []
             for spec in row.assessments:
@@ -149,11 +135,27 @@ class Dataset(BaseModel, Generic[InputsT, OutputT, MetadataT], extra='forbid'):
         path.write_text(content)
 
     @classmethod
+    def from_yaml(cls, path: Path | str = DEFAULT_DATASET_PATH) -> Self:
+        path = cls._get_relative_path(path)
+        if not path.exists():
+            raise FileNotFoundError(f'{cls.__name__} dataset file {path} does not exist')
+
+        raw = path.read_text()
+        loaded = yaml.safe_load(raw)
+        try:
+            result = cls.model_validate(loaded)
+            # result.rows = [cls.serialized_row_type().model_validate(row.model_dump()) for row in result.rows]
+        except ValidationError as e:
+            raise ValueError(
+                f'{cls.__name__} dataset file {path} contains data that does not match the schema:\n{e}.'
+            ) from e
+        return result
+
+    @classmethod
     def generate_dataset_files(
         cls,
         dataset_path: Path | str = DEFAULT_DATASET_PATH,
         schema_path: Path | str | None = None,
-        registry: AssessmentRegistry | None = None,
     ) -> str:
         dataset_path = cls._get_relative_path(dataset_path)
 
@@ -168,7 +170,7 @@ class Dataset(BaseModel, Generic[InputsT, OutputT, MetadataT], extra='forbid'):
         else:
             schema_path = cls._get_relative_path(schema_path)
 
-        schema_content = to_json(cls.type_adapter().json_schema(), indent=2).decode() + '\n'
+        schema_content = to_json(cls.model_json_schema_with_assessments(), indent=2).decode() + '\n'
         if not schema_path.exists() or schema_path.read_text() != schema_content:
             schema_path.write_text(schema_content)
 
@@ -194,42 +196,65 @@ class Dataset(BaseModel, Generic[InputsT, OutputT, MetadataT], extra='forbid'):
             dataset_path.write_text(f'{yaml_language_server_line}\n{content}')
         return schema_ref
 
-    # TODO: Implement type-safe JSON schema based on registered assessments
-    # @classmethod
-    # def model_json_schema_with_assertions(cls) -> dict[str, Any]:
-    #     registry = cls.inherited_assertion_registry()
-    #     tds: list[Any] = []
-    #     for name, function in registry.items():
-    #         signature = inspect.signature(function)
-    #         first_param = list(signature.parameters.values())[0]
-    #         type_hints = _typing_extra.get_function_type_hints(function)
-    #         type_hints.pop(first_param.name)
-    #         type_hints.pop('return', None)
-    #         for p in signature.parameters.values():
-    #             if p.default is not p.empty:
-    #                 type_hints[p.name] = NotRequired[type_hints[p.name]]
-    #         type_hints['call'] = Literal[name]
-    #         td = TypedDict(f'{function.__name__.title()}Assertion', type_hints)  # pyright : ignore
-    #         td.__pydantic_config__ = {'extra': 'forbid'}  # pyright : ignore
-    #         tds.append(td)
-    #
-    #     params: tuple[Any, ...] | None = None
-    #     for cls_ in cls.__mro__:
-    #         if issubclass(cls_, BaseModel) and cls_.__pydantic_generic_metadata__['args']:
-    #             params = cls_.__pydantic_generic_metadata__['args']
-    #             break
-    #     assert params is not None
-    #
-    #     class ClsDatasetRow(DatasetRow[params[0], params[1], params[2]]):
-    #         assertions: list[Union[tuple(tds)]]  # pyright : ignore
-    #     ClsDatasetRow.__name__ = cls.__name__ + 'Row'
-    #
-    #     class ClsDataset(cls):
-    #         rows: list[ClsDatasetRow[params[0], params[1], params[2]]]  # pyright : ignore
-    #
-    #     ClsDataset.__name__ = cls.__name__
-    #
-    #     return ClsDataset.model_json_schema()
+    @classmethod
+    def model_json_schema_with_assessments(cls) -> dict[str, Any]:
+        registry = cls.assessment_registry()
+
+        assessment_types: list[Any] = []
+        for name, function in registry.items():
+            signature = inspect.signature(function)
+            first_param = list(signature.parameters.values())[0]
+            type_hints = _typing_extra.get_function_type_hints(function)
+            type_hints.pop(first_param.name)
+            type_hints.pop('return', None)
+            required_type_hints: dict[str, Any] = {}
+
+            for p in signature.parameters.values():
+                if p.name == first_param.name:
+                    continue
+                if p.name not in type_hints:
+                    type_hints[p.name] = Any
+                if p.default is not p.empty:
+                    type_hints[p.name] = NotRequired[type_hints[p.name]]
+                else:
+                    required_type_hints[p.name] = type_hints[p.name]
+
+            if len(type_hints) == 0:
+                assessment_types.append(Literal[name])
+            elif len(type_hints) == 1:
+                type_hint_type = next(iter(type_hints.values()))  # pyright: ignore
+                td = TypedDict(f'short_assessment_{name}', {name: type_hint_type})  # pyright: ignore
+                td.__pydantic_config__ = {'extra': 'forbid'}  # pyright: ignore
+                assessment_types.append(td)
+            else:
+                if len(required_type_hints) == 1 and len(type_hints) > 1:
+                    # Short option: only one required parameter, so we can drop the nesting
+                    type_hint_type = next(iter(required_type_hints.values()))  # pyright: ignore
+                    td = TypedDict(f'short_assessment_{name}', {name: type_hint_type})  # pyright: ignore
+                    td.__pydantic_config__ = {'extra': 'forbid'}  # pyright: ignore
+                    assessment_types.append(td)
+
+                # Long form: multiple parameters, or multiple required parameters
+                params_td = TypedDict(f'assessment_params_{name}', type_hints)  # pyright: ignore
+                params_td.__pydantic_config__ = {'extra': 'forbid'}  # pyright: ignore
+                td = TypedDict(f'assessment_{name}', {name: params_td})  # pyright: ignore
+                td.__pydantic_config__ = {'extra': 'forbid'}  # pyright: ignore
+                assessment_types.append(td)
+
+        params = cls._params()
+
+        class ClsDatasetRow(DatasetRow[params[0], params[1], params[2]]):
+            assessments: list[Union[tuple(assessment_types)]] = Field(default_factory=list)  # pyright: ignore
+
+        ClsDatasetRow.__name__ = cls.__name__ + 'Row'
+
+        class ClsDataset(cls):
+            rows: list[ClsDatasetRow]  # pyright: ignore
+
+        ClsDataset.__name__ = cls.__name__
+
+        return ClsDataset.model_json_schema()
+
     # TODO: Always save a schema file when saving the dataset
 
     # TODO: Implement function to generate examples for a dataset using an LLM

fafbbf393f6d1b997119b785edae0ce65d66a623
David Montague <35119617+dmontagu@users.noreply.github.com>
2025-03-23 10:23:45 -0600

    Add a couple TODO comments

--- pydantic_evals/pydantic_evals/__init__.py
+++ pydantic_evals/pydantic_evals/__init__.py
@@ -9,6 +9,14 @@ TODO(DavidM): Implement a CLI with some pytest-like filtering API to make it eas
 # TODO: Question: How should we decorate functions to make it possible to eval them later?
 #  E.g., could use some kind of `eval_function` decorator, which ensures that calls to the function send eval-review-compatible data to logfire
 #  Basically we need to record the inputs and output. @logfire.instrument might be enough if we make it possible to record the output
+
+# TODO: Make logfire a non-required dependency, and document that it's needed for evaluators involving the span tree, and for recording LLM metrics
+# TODO: Rework so that:
+#   * EvaluatorFunction becomes EvaluatorFactory, which returns what is currently called a BoundEvaluatorFunction
+#       * the EvaluatorSpec "call" is used to look up the EvaluatorFactory, and its kwargs are used to call it and produce the BoundEvaluatorFunction
+#   * BoundEvaluatorFunction gets renamed to Evaluator
+#   * The EvaluatorSpec class becomes purely a (private?) means for serializing/deserializing Evaluator instances (with registered EvaluatorFactories)
+#   * The thing currently called Evaluator goes away and we assume that serializing/deserializing between Evaluator and EvaluatorSpec is idempotent
 from .dataset import Case, Dataset, increment_eval_metric
 from .reporting.reports import RenderNumberConfig, RenderValueConfig
 

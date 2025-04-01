f4000e69384190fd1978fe59d4156b7e52269a7e
David Montague <35119617+dmontagu@users.noreply.github.com>
2025-04-01 15:53:39 -0600

    Fix failing tests

--- tests/evals/utils.py
+++ tests/evals/utils.py
@@ -7,7 +7,7 @@ from rich.table import Table
 def render_table(table: Table) -> str:
     """Render a rich Table as a string."""
     string_io = StringIO()
-    Console(file=string_io).print(table)
+    Console(width=300, file=string_io).print(table)
     rendered = string_io.getvalue()
     # Need to trim end-of-line whitespace to prevent snapshot diffs after pre-commit removes the whitespace
     trimmed = '\n'.join([line.rstrip() for line in rendered.split('\n')])

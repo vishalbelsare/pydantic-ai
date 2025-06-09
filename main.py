8577ce2bb7e0311639cb7190c145fe0a46594f30
Marcelo Trylesinski <marcelotryle@gmail.com>
2025-06-08 17:15:41 -0700

    Create `pydantic_ai_sync`

--- main.py
+++ main.py
@@ -1,68 +1,66 @@
-import ast
-from pathlib import Path
-from typing import Any
+from typing import override
+
+import libcst as cst
+import libcst.matchers as m
+from libcst.codemod import CodemodContext, VisitorBasedCodemodCommand
+from libcst.codemod.visitors import AddImportsVisitor, RemoveImportsVisitor
+
+# 1. Transform `@asynccontextmanager` into `@contextmanager`.
+# 2. Transform `async def` into `def`.
+# 3. Transform `async with` into `with`.
+# 4. Transform `AsyncIterator` into `Iterator`.
+# 5. Remove `await`.
+
+# Questions:
+# 1. How do we solve MCP Servers?
 
 
-class AsyncToSyncTransformer(ast.NodeTransformer):
+class AsyncToSyncCommand(VisitorBasedCodemodCommand):
     """A transformer that converts async code to sync."""
 
-    def visit_AsyncFunctionDef(self, node: ast.AsyncFunctionDef):
-        # Create a new FunctionDef node with the same attributes as the AsyncFunctionDef node
-        new_decorator_list: list[ast.expr] = []
-        for decorator in node.decorator_list:
-            if isinstance(decorator, ast.Name) and decorator.id == 'asynccontextmanager':
-                decorator.id = 'contextmanager'
-            new_decorator_list.append(decorator)
-        new_node = ast.FunctionDef(
-            name=node.name,
-            args=node.args,
-            body=node.body,
-            decorator_list=new_decorator_list,
-            returns=node.returns,
-            type_comment=node.type_comment,
-            type_params=node.type_params,
-            **_extract_attributes(node),
-        )
-        self.generic_visit(new_node)
-        return new_node
+    DESCRIPTION = 'Converts async code to sync.'
 
-    def visit_AsyncWith(self, node: ast.AsyncWith):
-        new_node = ast.With(
-            items=node.items,
-            body=node.body,
-            type_comment=node.type_comment,
-            **_extract_attributes(node),
-        )
-        self.generic_visit(new_node)
-        return new_node
+    def __init__(self, context: CodemodContext) -> None:
+        super().__init__(context)
 
-    def visit_AsyncFor(self, node: ast.AsyncFor):
-        return self.generic_visit(node)
+    def leave_FunctionDef(
+        self, original_node: cst.FunctionDef, updated_node: cst.FunctionDef
+    ) -> cst.BaseStatement | cst.FlattenSentinel[cst.BaseStatement] | cst.RemovalSentinel:
+        if original_node.asynchronous:
+            return updated_node.with_changes(asynchronous=None)
+        return super().leave_FunctionDef(original_node, updated_node)
 
-    def visit_Await(self, node: ast.Await):
-        return self.generic_visit(node)
+    def leave_With(
+        self, original_node: cst.With, updated_node: cst.With
+    ) -> cst.BaseStatement | cst.FlattenSentinel[cst.BaseStatement] | cst.RemovalSentinel:
+        if original_node.asynchronous:
+            return updated_node.with_changes(asynchronous=None)
+        return super().leave_With(original_node, updated_node)
 
-    def visit_ImportFrom(self, node: ast.ImportFrom):
-        if node.module == 'contextlib':
-            for idx, alias in enumerate(node.names.copy()):
-                # Repeated imports are removed when ruff runs.
-                if alias.name in 'asynccontextmanager':
-                    node.names[idx].name = 'contextmanager'
-        return node
+    @override
+    def leave_For(
+        self, original_node: cst.For, updated_node: cst.For
+    ) -> cst.BaseStatement | cst.FlattenSentinel[cst.BaseStatement] | cst.RemovalSentinel:
+        if original_node.asynchronous:
+            return updated_node.with_changes(asynchronous=None)
+        return super().leave_For(original_node, updated_node)
 
+    def leave_Decorator(
+        self, original_node: cst.Decorator, updated_node: cst.Decorator
+    ) -> cst.Decorator | cst.FlattenSentinel[cst.Decorator] | cst.RemovalSentinel:
+        if m.matches(original_node, m.Decorator(decorator=m.Name('asynccontextmanager'))):
+            AddImportsVisitor.add_needed_import(self.context, 'contextlib', 'contextmanager')
+            RemoveImportsVisitor.remove_unused_import(self.context, 'contextlib', 'asynccontextmanager')
+            return updated_node.with_changes(decorator=cst.Name('contextmanager'))
+        return super().leave_Decorator(original_node, updated_node)
 
-def _extract_attributes(node: ast.stmt) -> dict[str, Any]:
-    return dict(
-        lineno=node.lineno,
-        col_offset=node.col_offset,
-        end_lineno=node.end_lineno,
-        end_col_offset=node.end_col_offset,
-    )
+    def leave_Name(self, original_node: cst.Name, updated_node: cst.Name) -> cst.BaseExpression:
+        if original_node.value == 'AsyncIterator':
+            AddImportsVisitor.add_needed_import(self.context, 'collections.abc', 'Iterator')
+            RemoveImportsVisitor.remove_unused_import(self.context, 'collections.abc', 'AsyncIterator')
+            return updated_node.with_changes(value='Iterator')
+        return super().leave_Name(original_node, updated_node)
 
-
-code = Path('pydantic_ai_slim/pydantic_ai/agent.py').read_text()
-tree = ast.parse(code)
-
-visitor = AsyncToSyncTransformer()
-transformed_tree = visitor.visit(tree)
-Path('agent.py').write_text(ast.unparse(transformed_tree))
+    @m.call_if_inside(m.Await(expression=m.Call()))
+    def leave_Await(self, original_node: cst.Await, updated_node: cst.Await) -> cst.BaseExpression:
+        return original_node.expression
